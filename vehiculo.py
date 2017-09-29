e1554e86b9b7a07cbfcc3936afa8efb33f7b5ecc
JONATHANHERNANDEZPHUNGCHINDA <jonathan.hernandez@edosoftfactory.com>
2017-09-29 13:49:12 +0100

    "Vroooom Vehicular"

---
+++ vehiculo.py
@@ -0,0 +1,10 @@
+class Vehiculo:
+    combustible = ""
+    caballos = 0
+    def __init__(self, combustible, caballos):
+        self.combustible = combustible
+        self.caballos = caballos
+
+    def imprimir(self):
+        print("Combustible: " + self.combustible + "Caballos: " + self.caballos)
+
