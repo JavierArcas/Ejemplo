e14f28d3a921ede4ed4560b76ad4f8b9892da298
JONATHANHERNANDEZPHUNGCHINDA <jonathan.hernandez@edosoftfactory.com>
2017-10-02 07:58:10 +0100

    Merge branch 'otro'

--- vehiculo.py
+++ vehiculo.py
@@ -2,13 +2,13 @@ class Vehiculo:
     combustible = ""
     caballos = 0
     direccionAsistida = None
-    ABS = None
+    abs = None
 
     def __init__(self, combustible, caballos, direccionasistida, ABS):
         self.combustible = combustible
         self.caballos = caballos
         self.direccionAsistida = direccionasistida
-        self.ABS = ABS
+        self.abs = ABS
 
     def imprimir(self):
         print("Combustible: " + self.combustible + "Caballos: " + self.caballos)
