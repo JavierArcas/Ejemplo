0c43e267543c17f525330e49850774a13855bcb6
JONATHANHERNANDEZPHUNGCHINDA <jonathan.hernandez@edosoftfactory.com>
2017-10-02 07:54:03 +0100

    "Añadidos elementos a Vroooom Vehicular"

--- vehiculo.py
+++ vehiculo.py
@@ -1,9 +1,14 @@
 class Vehiculo:
     combustible = ""
     caballos = 0
-    def __init__(self, combustible, caballos):
+    direccionAsistida = None
+    ABS = None
+
+    def __init__(self, combustible, caballos, direccionasistida, ABS):
         self.combustible = combustible
         self.caballos = caballos
+        self.direccionAsistida = direccionasistida
+        self.ABS = ABS
 
     def imprimir(self):
         print("Combustible: " + self.combustible + "Caballos: " + self.caballos)
